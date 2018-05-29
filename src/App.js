fb38d664dc808aec6d8915508b45f57ca7e7a466
Riti Sachdeva <ritiallstars@gmail.com>
2018-05-29 15:08:27 -0400

    CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Route, Switch, NavLink} from 'react-router-dom' 
+import { Route, Switch, NavLink } from 'react-router-dom'
 
 import './App.css'
 
@@ -11,19 +11,17 @@ class App extends Component {
           <h3>Ain't no party like an</h3>
           <h1>API Party</h1>
         </div>
-          <ul>
-            <li>
-              <NavLink to="/github">GitHub API</NavLink>
-            </li>
-          </ul>
+        <ul className="navLinks">
+          <li>
+            <NavLink to="/github">GitHub API</NavLink>
+          </li>
+        </ul>
         <Switch>
-          <Route path="/github" render={() => <h1>GitHub!</h1>} />
-          <Route render={() => <p>To get started, click one of the links above</p>} />
+          <Route path="/github" render={() => <h1>Github!</h1>} />
+          <Route render={() => <p>To get started, click one of the links above.</p>} />
         </Switch>
       </div>
-
     )
   }
 }
-
-export default App;
+export default App
